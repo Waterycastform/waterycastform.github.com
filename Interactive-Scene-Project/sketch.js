5f9facca6fea2e63cdf0702c103c57e910a365c9
Waterycastform <114016086+Waterycastform@users.noreply.github.com>
2022-09-21 11:53:18 -0600

    fixed silly repo name

--- Interactive-Scene-Project/sketch.js
+++ Interactive-Scene-Project/sketch.js
@@ -1,15 +1,25 @@
-// Project Title
-// Your Name
-// Date
+// Interactive Scene
+// Saabir Yousuf
+// Sept. 21st, 2022
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let palace;
+
+function preLoad() {
+  palace = loadImage("Buckingham.jpg");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  openScreen();
 }
 
 function draw() {
   background(220);
+}
+
+function openScreen () {
+  Image (palace, windowWidth, windowHeight);
 }
