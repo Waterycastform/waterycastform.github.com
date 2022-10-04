d9bf98dfde8cb52a9ea84683f0474a95694c320e
Waterycastform <114016086+Waterycastform@users.noreply.github.com>
2022-10-04 12:16:08 -0600

    added button

--- Interactive-Scene-Project/sketch.js
+++ Interactive-Scene-Project/sketch.js
@@ -14,6 +14,9 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  textAlign(CENTER, CENTER);
+  
 }
 
 function draw() {
@@ -25,6 +28,12 @@ function draw() {
 
 function openScreen () {
   image(palace, 0, 0, windowWidth, windowHeight);
-  fill ("black");
-  rect(400, 600, width - 400, height - 200);
+  fill (200, 100, 200, 150);
+  strokeWeight(4);
+  rect(windowWidth/2, windowHeight*0.75, windowWidth/2, windowHeight*0.15, 20);
+  fill("black");
+  textSize( (windowHeight+windowWidth)/40);
+  textStyle(BOLD);
+  text("New Monarch!", windowWidth/2, windowHeight*0.75);
+
 }
